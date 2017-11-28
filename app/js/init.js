ac538bb3e836a53d4ff8c644adbdad15155a04df
maxdanichev@gmail.com <Flink96>
2017-11-28 22:52:28 +0200

    Сделал адаптив

--- app/js/init.js
+++ app/js/init.js
@@ -46,4 +46,68 @@ $( function() {
 
     $('select').styler();
 
+    //scrollbar init
+    $('.scrollbar-inner').scrollbar();
+
+    //chart init
+    var chartBlock = document.getElementById('myChart');
+    if(chartBlock){
+        var myChart= new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets:[
+                    {
+                label: 'Signups',
+                data: [100,200,220,170,130,90,154,180,130],
+                        backgroundColor: 'rgba[128,191,202,1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200,120,180,140,120,140,104,150,100],
+                        backgroundColor: 'rgba[255,154,56,1)'
+                    },
+                    {
+                        label: 'Earned',
+                        data: [50,60,80,40,20,240,199,110,10],
+                        backgroundColor: '#59b66d'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales:{
+                    yAsex: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
+
+    //range slider init
+
+    var handle = $('#custom-handle');
+    var range = $('#slider');
+
+    range.slider({
+        value: 50,
+        orientation: "horizontal",
+        range: "min",
+        animate: true,
+        max: 200,
+        
+        create: function (event, ui) {
+            handle.append('<span class="value">' + $( this ).slider("value") + 'hours</span>');
+            var value = handle.find('.value');
+            range.slider('option','valueSpan', value);
+        },
+        slide: function ( event, ui ) {
+          range.slider("option", "valueSpan").text(ui.value + 'hours');
+        }
+    })
+    
+    console.log(handle,slider);
 } );
